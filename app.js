7f9c7c9b1402a7bf9fc6bab76542c6e0e552368f
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2021-12-14 12:43:40 +0100

    changes 1

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
 /* zwischen den verschiedenen elementen wechseln und das in der url anzeigen */
 function myFunction(evt, navName) {
     /* Variablen */
-    var i, tabcontent, tablinks;
+    var i, navcontent, navbar2;
 
     /* alle Elemente mit class="navcontent" */
-    tabcontent = document.getElementsByClassName("navcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+    navcontent = document.getElementsByClassName("navcontent");
+    for (i = 0; i < navcontent.length; i++) {
+        navcontent[i].style.display = "none";
     }
 
     /* alle Elemente mit class="navbar" */
-    tablinks = document.getElementsByClassName("navbar");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    navbar2 = document.getElementsByClassName("navbar2");
+    for (i = 0; i < navbar2.length; i++) {
+        navbar2[i].className = navbar2[i].className.replace(" active", "");
     }
 
     /* jetztigen tab anzeigen */
     document.getElementById(navName).style.display = "block";
     evt.currentTarget.className += " active";
 
-    /* responsivness implementieren */
+    /* responsivness implementieren
     var x = document.getElementById("navbar1");
     if (x.className === "navbar") {
         x.className += " responsive";
     } else {
         x.className = "navbar";
     }
+    */
 }
