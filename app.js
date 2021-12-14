1c0854833ea066fef16ec84cf493b2c421f31d0d
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2021-12-14 14:49:47 +0100

    buttons funktionieren jetzt

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
 // Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
-/* zwischen den verschiedenen elementen wechseln und das in der url anzeigen */
-function myFunction(evt, navName) {
-    /* Variablen */
-    var i, navcontent, navbar2;
-
-    /* alle Elemente mit class="navcontent" */
-    navcontent = document.getElementsByClassName("navcontent");
-    for (i = 0; i < navcontent.length; i++) {
-        navcontent[i].style.display = "none";
+function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // alle Elemente von "tabcontent"
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-
-    /* alle Elemente mit class="navbar" */
-    navbar2 = document.getElementsByClassName("navbar2");
-    for (i = 0; i < navbar2.length; i++) {
-        navbar2[i].className = navbar2[i].className.replace(" active", "");
+  
+    // alle Elemente von "tablinks"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    /* jetztigen tab anzeigen */
-    document.getElementById(navName).style.display = "block";
+  
+    // Tab anzeigen
+    document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-
-    /* responsivness implementieren
-    var x = document.getElementById("navbar1");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-    } else {
-        x.className = "navbar";
-    }
-    */
-}
+  }
