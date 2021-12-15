9634405b23b9731d417bc4449b46d5d94d471800
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2021-12-15 13:42:31 +0100

    back button gemacht

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
+// Get the element with id="defaultOpen1" and click on it
+document.getElementById("defaultOpen1").click();
 
 //function to open a tab
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
-  
+
     // alle Elemente von "tabcontent"
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
+
     // alle Elemente von "tablinks"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
+
     // Tab anzeigen
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
 
-  //function to prepare for banana game
+//function to prepare for banana game
