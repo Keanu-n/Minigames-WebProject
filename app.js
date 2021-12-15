8f39a3d318233d2a5a4e36c5fc2d80d7ef83881e
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2021-12-15 14:37:20 +0100

    animation vorbereitet

--- app.js
+++ app.js
@@ -22,5 +22,3 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
-
-//function to prepare for banana game
