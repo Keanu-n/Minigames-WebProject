362737531483e27d936209fc96843c603e8d8082
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2021-12-22 14:41:45 +0100

    banane bewegt sich

--- app.js
+++ app.js
@@ -5,23 +5,40 @@ document.getElementById("defaultOpen").click();
 function openTab(evt, tabName) {
     // Declare all variables
     var i, tabcontent, tablinks;
-  
+
     // alle Elemente von "tabcontent"
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+        tabcontent[i].style.display = "none";
     }
-  
+
     // alle Elemente von "tablinks"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-  
+
     // Tab anzeigen
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-  }
+}
 
-  //function to prepare for banana game
-  
+//function to prepare for banana game
+var id = null;
+
+function banana() {
+    var elem = document.getElementById("bananaAnimation2");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
