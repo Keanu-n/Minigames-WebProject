ee340d3927b94127f292c410fcc49aeaee317517
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2022-01-08 16:18:04 +0100

    symbol game weiter

--- app.js
+++ app.js
@@ -47,8 +47,10 @@ function banana() {
     }
 }
 
+//symbol game functions
+
 function symbolsHide() {
-    var images;
+    var images, i;
     images = document.getElementsByClassName("symbolPictures");
     for (i = 0; i < images.length; i++) {
         images[i].style.display = "none";
@@ -56,12 +58,93 @@ function symbolsHide() {
 }
 
 function symbol() {
-    var images;
+    var images, i;
     images = document.getElementsByClassName("symbolPictures");
     for (i = 0; i < images.length; i++) {
         images[i].style.display = "block";
     }
 }
+
+var notClicked = true;
+var symbolStar = false,
+    symbolVader = false,
+    symbolMoon = false,
+    symbolTrain = false;
+
+document.getElementById("symbolStar2").onclick = clicked1();
+document.getElementById("symbolVader2").onclick = clicked2();
+document.getElementById("symbolMoon2").onclick = clicked3();
+document.getElementById("symbolTrain2").onclick = clicked4();
+
+function clicked1() {
+    notClicked = false;
+    symbolStar = true;
+}
+
+function clicked2() {
+    notClicked = false;
+    symbolVader = true;
+}
+
+function clicked3() {
+    notClicked = false;
+    symbolMoon = true;
+}
+
+function clicked4() {
+    notClicked = false;
+    symbolTrain = true;
+}
+
+function symbolGame() {
+    var images, i, randTime, randImage, time, count = 0;
+    images = document.getElementsByClassName("symbolPictures");
+
+    for (i = 0; i < 20; i++) {
+        time = 0;
+        randTime = Math.round((Math.random() * 10) + 1);
+        randImage = Math.round((Math.random() * 10) / 4);
+
+        setTimeout(moveOn, randTime * 1000);
+
+        function moveOn() {
+            if (randImage == 0 || randImage == 1) {
+                images[0].style.display = "block";
+            } else if (randImage == 2) {
+                images[1].style.display = "block";
+            } else if (randImage == 3) {
+                images[2].style.display = "block";
+            } else {
+                images[3].style.display = "block";
+            }
+
+            while (notClicked == true) {}
+
+            if (symbolStar == true && (randImage == 1 || randImage == 0)) {
+                count++;
+            } else if (symbolVader == true && randImage == 2) {
+                count++;
+            } else if (symbolMoon == true && randImage == 3) {
+                count++;
+            } else if (symbolTrain == true && randImage == 4) {
+                count++;
+            }
+
+            document.getElementById("SymbolCounter").innerHTML = count + " Punkte, weiter so!";
+            notClicked = true;
+
+            symbolStar = false;
+            symbolVader = false;
+            symbolMoon = false;
+            symbolTrain = false;
+
+            symbolsHide();
+        }
+    }
+
+}
+
+//click Game functions
 
 var count = 0;
 var start = false;
