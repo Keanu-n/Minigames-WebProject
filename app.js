d51df977f29b991f993c41ee9b59a640d542d51c
Keanu-n <78546527+Keanu-n@users.noreply.github.com>
2022-01-18 07:58:01 +0100

    Nicht noch einmal rauslöschen!!!!!!!

--- app.js
+++ app.js
@@ -87,3 +87,217 @@ function move(event) {
         }
     }
 }
+
+//TicTacToe
+
+var btns_active = false, player = 0;
+var one1 = false, oneX = 0,
+    two1 = false, twoX = 0,
+    three1 = false, threeX = 0,
+    four1 = false, fourX = 0,
+    five1 = false, fiveX = 0,
+    six1 = false, sixX = 0,
+    seven1 =  false, sevenX = 0,
+    eight1 = false, eightX = 0,
+    nine1 = false, nineX = 0;
+
+function ticTacToe_start(){
+    var buttons;
+    buttons = document.getElementsByClassName("tic_btn");
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].style.backgroundColor = "white";
+    }
+    document.getElementById("gewinnen1").style.display = "none";
+    btns_active = true;
+
+    one1 = false;
+    two1 = false;
+    three1 = false;
+    four1 = false;
+    five1 = false;
+    six1 = false;
+    seven1 = false;
+    eight1 = false;
+    nine1 = false;
+
+    player = 0;
+
+    //Start button muss verschwinden...
+    document.getElementById("aaaaah").style.display = "none";
+}
+
+//überprüfen ob schon jemand gewonnen hat (alle Möglichkeiten durchlaufen)
+function gewonnen(){
+    player = player + 1;
+    console.log(player);
+    if(oneX == 1 && twoX == 1 && threeX == 1){
+        win(1);
+    }else if(oneX == 2 && twoX == 2 && threeX == 2){
+        win(2);
+    }else if(fourX == 1 && fiveX == 1 && sixX == 1){
+        win(1);
+    }else if(fourX == 2 && fiveX == 2 && sixX == 2){
+        win(2);
+    }else if(sevenX == 1 && eightX == 1 && nineX == 1){
+        win(1);
+    }else if(sevenX == 2 && eightX == 2 && nineX == 2){
+        win(2);
+    }else if(oneX == 1 && fourX == 1 && sevenX == 1){
+        win(1);
+    }else if(oneX == 2 && fourX == 2 && sevenX == 2){
+        win(2);
+    }else if(twoX == 1 && fiveX == 1 && eightX == 1){
+        win(1);
+    }else if(twoX == 2 && fiveX == 2 && eightX == 2){
+        win(2);
+    }else if(threeX == 1 && sixX == 1 && nineX == 1){
+        win(1);
+    }else if(threeX == 2 && sixX == 2 && nineX == 2){
+        win(2);
+    }else if(oneX == 1 && fiveX == 1 && nineX == 1){
+        win(1);
+    }else if(oneX == 2 && fiveX == 2 && nineX == 2){
+        win(2);
+    }else if(threeX == 1 && fiveX == 1 && sevenX == 1){
+        win(1);
+    }else if(threeX == 2 && fiveX == 2 && sevenX == 2){
+        win(2);
+    }
+}
+
+function win(i){
+    console.log("gewonnen");
+    if(i == 1){
+        document.getElementById("gewinnen1").innerHTML ="gelb gewinnt!";
+    }else{
+        document.getElementById("gewinnen1").innerHTML ="orange gewinnt!";
+    }
+    document.getElementById("gewinnen1").style.display = "block";
+    btns_active = false;
+}
+
+function one(){
+    if(btns_active == true && one1 == false){
+        if(player % 2 == 0){
+            document.getElementById("one2").style.backgroundColor = "yellow";
+            oneX = 1;
+        }else{
+            document.getElementById("one2").style.backgroundColor = "orange";
+            oneX = 2;
+        }
+        one1 = true;
+        gewonnen();
+    }
+}
+
+function two(){
+    if(btns_active == true && two1 == false){
+        if(player % 2 == 0){
+            document.getElementById("two2").style.backgroundColor = "yellow";
+            twoX = 1;
+        }else{
+            document.getElementById("two2").style.backgroundColor = "orange";
+            twoX = 2;
+        }
+        two1 = true;
+        gewonnen();
+    }
+}
+
+function three(){
+    if(btns_active == true && three1 == false){
+        if(player % 2 == 0){
+            document.getElementById("three2").style.backgroundColor = "yellow";
+            threeX = 1;
+        }else{
+            document.getElementById("three2").style.backgroundColor = "orange";
+            threeX = 2;
+        }
+        three1 = true;
+        gewonnen();
+    }
+}
+
+function four(){
+    if(btns_active == true && four1 == false){
+        if(player % 2 == 0){
+            document.getElementById("four2").style.backgroundColor = "yellow";
+            fourX = 1;
+        }else{
+            document.getElementById("four2").style.backgroundColor = "orange";
+            fourX = 2;
+        }
+        four1 = true;
+        gewonnen();
+    }
+}
+
+function five(){
+    if(btns_active == true && five1 == false){
+        if(player % 2 == 0){
+            document.getElementById("five2").style.backgroundColor = "yellow";
+            fiveX = 1;
+        }else{
+            document.getElementById("five2").style.backgroundColor = "orange";
+            fiveX = 2;
+        }
+        five1 = true;
+        gewonnen();
+    }
+}
+
+function six(){
+    if(btns_active == true && six1 == false){
+        if(player % 2 == 0){
+            document.getElementById("six2").style.backgroundColor = "yellow";
+            sixX = 1;
+        }else{
+            document.getElementById("six2").style.backgroundColor = "orange";
+            sixX = 2;
+        }
+        six1 = true;
+        gewonnen();
+    }
+}
+
+function seven(){
+    if(btns_active == true && seven1 == false){
+        if(player % 2 == 0){
+            document.getElementById("seven2").style.backgroundColor = "yellow";
+            sevenX = 1;
+        }else{
+            document.getElementById("seven2").style.backgroundColor = "orange";
+            sevenX = 2;
+        }
+        seven1 = true;
+        gewonnen();
+    }
+}
+
+function eight(){
+    if(btns_active == true && eight1 == false){
+        if(player % 2 == 0){
+            document.getElementById("eight2").style.backgroundColor = "yellow";
+            eightX = 1;
+        }else{
+            document.getElementById("eight2").style.backgroundColor = "orange";
+            eightX = 2;
+        }
+        eight1 = true;
+        gewonnen();
+    }
+}
+
+function nine(){
+    if(btns_active == true && nine1 == false){
+        if(player % 2 == 0){
+            document.getElementById("nine2").style.backgroundColor = "yellow";
+            nineX = 1;
+        }else{
+            document.getElementById("nine2").style.backgroundColor = "orange";
+            nineX = 2;
+        }
+        nine1 = true;
+        gewonnen();
+    }
+}
